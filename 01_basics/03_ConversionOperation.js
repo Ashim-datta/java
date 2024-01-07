0b3c665aafe76e527874c29056358bfbc602a92d
Ashim-datta <154074655+Ashim-datta@users.noreply.github.com>
2024-01-07 05:13:19 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,15 @@
+// let score = true
+// console.log(typeof score);
+// let valnumber = Number(score)
+// console.log(valnumber);
+
+// let isLoggedIn = 0
+// let BooleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof BooleanisLoggedIn);
+// console.log(BooleanisLoggedIn);
+
+let somenumber = 30
+console.log(typeof somenumber);
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
